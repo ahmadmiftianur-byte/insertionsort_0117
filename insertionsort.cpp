cf6dbe162dd33ada008660075b1d41af5c0ee5f9
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-03-12 12:07:40 +0700

    menambahkan prosedur insertionsort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -25,4 +25,34 @@ void input()
     cout << "============Masukkan Elemet Array==========" << endl;
     cout << "===========================================" << endl;
 
-    
+    for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
+    {
+        cout << "Data ke-" << (i + 1) << ": "; // Memasukan atau menginputkan nilai data n
+        cin >> arr[i];                         // Menyimpan nilai data n kedalam array arr
+    }
+}
+
+void insertionSort()
+{             // Prosedure InsertionSort
+    int temp; // Membuat variable data temporer atau penyimpanan sementara
+    int j;    // Membuat variable j sebagai penanda
+
+    for (int i = 0; i < n ; i++)
+    {                  // step 1
+        temp = arr[i]; // step 2
+        j = i - 1;     // step 3
+
+        while (j >= 0 && arr[j] > temp) // step 4
+        {
+            arr[j + 1] = arr[j]; // step 4a
+            j--;                 // step 4b
+        }
+        arr[j + 1] = temp; // step 5
+        cout << "\nstep " << i << ": "; // Output step ke layar
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " "; // Output ke layar
+        }
+    }
+}
+
