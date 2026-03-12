678884e3a0e38506c732572d2ed269ad3c932a1a
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-03-12 12:05:30 +0700

    menambahkan prosedur inpot

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -4,3 +4,20 @@ using namespace std;
 int arr[20]; // Membuat array dengan panjang data 20
 int n;       // Membuat variable inputan n
 
+void input()
+{ // Prosedure Input
+    while (true)
+    {
+        cout << "Masukkan Jumlah data pada Array : "; // Membuat inputan jumlah element Array
+        cin >> n;                                     // Memanggil variable n
+
+        if (n <= 20)
+        { // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray tidak boleh lebih dari 20\n"; // Menampilkan pesan jika data lebih dari 20
+        }
+    }
+    
